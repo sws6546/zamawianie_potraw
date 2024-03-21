12496408e67aa845e5de66012a43eb26c6f73e53
Szymon <skimpywhell6546@gmail.com>
2024-03-21 20:00:04 +0100

    tailwind

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express()
 const port = 3000
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.json()) 
 app.use(express.json());
 
 app.get('/', (req, res) => {
